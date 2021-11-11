045e9ab5d7478fbb0d157972f86d7f8694e698a9
Freizeit-sys <work16087y0rha@gmail.com>
2021-11-11 16:03:31 +0900

    recreate .gitignore

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+		<div class="d-flex flex-row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-body">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
